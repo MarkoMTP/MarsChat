fd5c1db02d3645e2d85d8d0818631ad172273a47
MarkoMTP <marko.mtp8@gmail.com>
2025-07-11 09:45:38 +0200

    added inboxes section and given tests

---
+++ marschat-frontend/src/components/InboxesSection.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+import InboxComponent from "./InboxComponent";
+
+export default function InboxesSection({ inboxes }) {
+  return (
+    <div>
+      <h2>Inboxes Section</h2>
+      <div>
+        {inboxes.map((inbox) => (
+          <InboxComponent key={inbox.id} inbox={inbox} onClick={() => {}} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ marschat-frontend/src/tests/inboxSection.test.jsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import InboxesSection from "../components/InboxesSection.jsx";
+import jwtDecode from "jwt-decode";
+
+vi.mock("jwt-decode", () => ({
+  default: vi.fn(),
+}));
+
+describe("Inboxes Section", () => {
+  beforeEach(() => {
+    vi.stubGlobal("localStorage", {
+      getItem: vi.fn(() => "FAKETOKEN"),
+    });
+
+    jwtDecode.mockReturnValue({ id: "1" });
+  });
+
+  it("Renders all the inboxes and open chat buttons", () => {
+    const Inboxes = [
+      {
+        id: "1",
+        name: "Marko",
+      },
+      {
+        id: "2",
+        name: "Luka",
+      },
+      {
+        id: "3",
+        name: "Roko",
+      },
+    ];
+
+    render(<InboxesSection inboxes={Inboxes} />);
+
+    expect(screen.getByText("Inboxes Section")).toBeInTheDocument();
+
+    expect(screen.getByText(/Marko/i)).toBeInTheDocument();
+    expect(screen.getByText(/Roko/i)).toBeInTheDocument();
+    expect(screen.getByText(/Luka/i)).toBeInTheDocument();
+
+    const buttons = screen.getAllByRole("button", { name: /open chat/i });
+
+    expect(buttons).toHaveLength(3);
+  });
+});
