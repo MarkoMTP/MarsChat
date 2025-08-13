94ca628220596a0216d82c6dcaa42081bd6de27f
MarkoMTP <marko.mtp8@gmail.com>
2025-08-13 12:20:48 +0200

    added a new middleware function that sets the given chat via click of the send message button

---
+++ marschat-frontend/src/middleware/openChatFromSendMessage.js
@@ -0,0 +1,13 @@
+import api from "../api";
+
+export default async function openChatFromSendMessage(userId, setOpenChat) {
+  try {
+    const inbox = await api.get(`/inbox/direct/${userId}`);
+
+    setOpenChat(inbox.data);
+  } catch (err) {
+    console.error("Failed to setOpenChat messages:", err);
+  }
+}
+
+//moras ovo git add
