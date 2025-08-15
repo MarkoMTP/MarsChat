41bcd61e02f170c5e6a3de84eb9b7a07d4ab8368
MarkoMTP <marko.mtp8@gmail.com>
2025-08-15 15:44:20 +0200

    added new parameters for userSection

--- marschat-frontend/src/components/UsersSection.jsx
+++ marschat-frontend/src/components/UsersSection.jsx
@@ -1,13 +1,18 @@
 import UserComponent from "./UserComponent";
+import openChatFromSendMessage from "../middleware/openChatFromSendMessage";
 
-export default function UsersSection({ users }) {
+export default function UsersSection({ users, setOpenChat }) {
   return (
     <div>
       <h2>Users Section</h2>
       <div>
         {Array.isArray(users) && users.length > 0 ? (
           users.map((user) => (
-            <UserComponent key={user.id} user={user} onClick={() => {}} />
+            <UserComponent
+              key={user.id}
+              user={user}
+              onClick={() => openChatFromSendMessage(user.id, setOpenChat)}
+            />
           ))
         ) : (
           <p>No users </p>
